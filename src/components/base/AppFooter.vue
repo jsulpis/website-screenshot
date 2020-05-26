b66b46578401ddcd3c2ddbb828ac568ece4b9cf3
Julien Sulpis <julien@MacBook-Pro-Julien.local>
2020-05-26 22:32:52 +0200

    fix: css class purged in functional component

--- src/components/base/AppFooter.vue
+++ src/components/base/AppFooter.vue
@@ -4,19 +4,19 @@
       <div class="flex flex-wrap items-center justify-around mx-auto lg:w-2/3">
         <app-lang-select class="w-full mb-2 sm:w-auto" />
         <app-theme-select class="w-full mb-2 sm:w-auto" />
-        <footer-link class="block w-full mb-2 sm:w-auto" href="https://github.com/jsulpis/website-screenshot">
+        <app-footer-link class="block w-full mb-2 sm:w-auto" href="https://github.com/jsulpis/website-screenshot">
           {{ $t("footer.star-on-github") }}
-        </footer-link>
+        </app-footer-link>
       </div>
       <hr class="mt-3 mb-6 border-gray-400" />
       <div class="w-full mx-auto">
         <p class="py-1 text-sm">
           ©{{ new Date().getFullYear() }} {{ $t("footer.made-with") }} <FontAwesomeIcon icon="heart" size="xs" />
-          {{ $t("footer.by") }} <footer-link href="https://github.com/jsulpis">Julien Sulpis</footer-link>
+          {{ $t("footer.by") }} <app-footer-link href="https://github.com/jsulpis">Julien Sulpis</app-footer-link>
         </p>
         <p class="text-xs">
           {{ $t("footer.project-bootstrapped-with") }}
-          <footer-link href="https://nuxtjs-template.netlify.app">{{ $t("footer.nuxt-template") }}</footer-link>
+          <app-footer-link href="https://nuxtjs-template.netlify.app">{{ $t("footer.nuxt-template") }}</app-footer-link>
         </p>
       </div>
     </div>
@@ -26,21 +26,13 @@
 <script>
 import AppThemeSelect from "@/components/base/AppThemeSelect.vue";
 import AppLangSelect from "@/components/base/AppLangSelect.vue";
-
-const FooterLink = {
-  functional: true,
-  render: (h, { data, children }) => (
-    <a href={data.attrs.href} class={`font-semibold hover:opacity-75 ${data.staticClass}`}>
-      {children}
-    </a>
-  )
-};
+import AppFooterLink from "@/components/base/AppFooterLink";
 
 export default {
   components: {
-    FooterLink,
     AppThemeSelect,
-    AppLangSelect
+    AppLangSelect,
+    AppFooterLink
   }
 };
 </script>
